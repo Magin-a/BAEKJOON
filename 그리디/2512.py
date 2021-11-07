3a1181c6e23de2a9bbbc41e84da346ae92ae837b
Magin-a <80820166+Magin-a@users.noreply.github.com>
2021-11-08 00:50:44 +0900

    Update 2512.py

--- 그리디/2512.py
+++ 그리디/2512.py
@@ -24,5 +24,52 @@ else:
     print(result)
 
 
+#예산
+import sys
+n = int(sys.stdin.readline()) #n은 3 이상 10,000 이하
+money = list(map(int, sys.stdin.readline().split()))
+max_money = int(sys.stdin.readline()) # Max은 n 이상 1,000,000,000 이하
 
+if sum(money) <= max_money:
+    print(max(money))
+
+else:
+    rev_money = max_money//n
+    demand_money = sum(money)//n
+    tol_money = sum(money)
+    
+
+    while tol_money >= max_money:
+        corret = 0
+        count = 0 
+        for i in money:
+            if i < rev_money:
+                corret += i
+
+            else:
+                corret += rev_money
+                count += 1
+
+        tol_money -= count 
+        if tol_money >= corret:
+            rev_money +=1
+        else:
+            rev_money -= 1
+    
+    print(rev_money)
+#2안    
+    min_num, max_num = 0, max(money)
+
+    while min_num <= max_num:
+        mid = (min_num+max_num)//2
+        correct = 0
+        for i in money:
+            correct += min(i, mid)
+
+        if correct < max_money:
+            min_num = mid+1
+        else:
+            max = mid -1
+        
+    print(mid)
 

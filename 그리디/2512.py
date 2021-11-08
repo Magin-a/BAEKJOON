640d9e51a41fc163efb2df8387c7551e92abba54
Magin-a <80820166+Magin-a@users.noreply.github.com>
2021-11-09 01:13:47 +0900

    예산

--- 그리디/2512.py
+++ 그리디/2512.py
@@ -4,72 +4,25 @@ n = int(sys.stdin.readline()) #n은 3 이상 10,000 이하
 money = list(map(int, sys.stdin.readline().split()))
 max_money = int(sys.stdin.readline()) # Max은 n 이상 1,000,000,000 이하
 
-
-if sum(money) <= max_money:  #총 예산이 max값보다 작다면 가장 큰 예산 출력
+if sum(money) <= max_money:# 요청 예산의 총합 < 가능한 총예산일 경우 요청 예산의 가장 큰 액수 출력
     print(max(money))
 
 else:
-    test_num = sum(money)//n
-    money.append(test_num)
-    money.sort()
-
-    a = money.index(test_num)
-    del(money[a])
-
-    for x in money[:a]:
-        if x <= test_num:
-            max_money -= x
-       
-    result = int(max_money/(n-(a)))
-    print(result)
-
-
-#예산
-import sys
-n = int(sys.stdin.readline()) #n은 3 이상 10,000 이하
-money = list(map(int, sys.stdin.readline().split()))
-max_money = int(sys.stdin.readline()) # Max은 n 이상 1,000,000,000 이하
-
-if sum(money) <= max_money:
-    print(max(money))
-
-else:
-    rev_money = max_money//n
-    demand_money = sum(money)//n
-    tol_money = sum(money)
+    min_num, max_num = 0, max(money) 
     
+    while max_num >= min_num: # max_money에 도달할때까지 반복
+        mid = (max_num+min_num)//2 # 기준점 생성
+        correct = 0 #반복문 중 예산을 저장
 
-    while tol_money >= max_money:
-        corret = 0
-        count = 0 
-        for i in money:
-            if i < rev_money:
-                corret += i
+        for i in money: #반복문으로 예산 배정
+            if mid >= i: #기준점보다 작은 값은 예산 통과
+                correct += i
+            else: #문제가 되는 예산은 한계선인 mid으로 대체
+                correct += mid
 
-            else:
-                corret += rev_money
-                count += 1
+        if correct > max_money: #배정 예산 > 가능한 총 예산 일 경우 
+            max_num = mid -1  #가장 큰 값을 -1 해준다
 
-        tol_money -= count 
-        if tol_money >= corret:
-            rev_money +=1
-        else:
-            rev_money -= 1
-    
-    print(rev_money)
-#2안    
-    min_num, max_num = 0, max(money)
-
-    while min_num <= max_num:
-        mid = (min_num+max_num)//2
-        correct = 0
-        for i in money:
-            correct += min(i, mid)
-
-        if correct < max_money:
-            min_num = mid+1
-        else:
-            max = mid -1
-        
-    print(mid)
-
+        else:  #배정예산 < 가능한 총 예산 일 경우  
+            min_num = mid +1 # 가장 작은 값에서 +1 해준다
+    print(max_num) #while 종료후 값 출력

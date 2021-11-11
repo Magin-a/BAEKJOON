bb1d94f9649fa86cc45a84a4dc7a17fc55aa9391
Magin-a <80820166+Magin-a@users.noreply.github.com>
2021-11-11 15:06:22 +0900

    Update 13335.py

--- 구현/13335.py
+++ 구현/13335.py
@@ -1,31 +1,23 @@
-#트럭
 import sys
+from collections import deque
+
 n, w, l = map(int, sys.stdin.readline().split())
-truck = list(map(int, sys.stdin.readline().split()))
-bridge = [0 for _ in range(w)]
-cnt = 0 # cnt번쨰 트럭
-time = 0 #최종 걸리는 시간
+truck = deque(list(map(int, sys.stdin.readline().split())))
+bridge = deque(0 for _ in range(w))
+count = 0  #최종 트럭이 다리를 건너는데 걸리는 시간
 
-for i in truck:
-    okay = 0
-    zero_count = 0
-    while okay != 1:
-        if sum(bridge)+i < l:
-            bridge.pop(0)
-            bridge.append(i)
-            time += 1
-            okay = 1 #해당 트럭이 다리에 올라갔을 때 1
-            cnt += 1
-            print(bridge)
+while truck: #트럭의 있는동안 반복
+    bridge.popleft() #다리 위에 한칸씩 이동
+    if sum(bridge) + truck[0] <= l: #다리의 최대무게가 넘지 않는다면 트럭 추가
+        bridge.append(truck.popleft())
+        count += 1 #이동시간 +1
+    
+    else: #다리의 최대 무게가 넘는다면 0추가
+        bridge.append(0) 
+        count += 1 
 
-        else:
-            zero_count += 1 
-            bridge.pop(0)
-            bridge.append(0)
-            time += 1
-            print(bridge)
-    if cnt == n:
-        time += w 
-
-time = time - zero_count
-print(time)
+if sum(bridge) > 0: #다리 위에 트럭이 남아 있다면 다리를 건널 때까지pop
+    while sum(bridge) != 0:
+        bridge.popleft()
+        count +=1
+print(count) #최종 걸린시간

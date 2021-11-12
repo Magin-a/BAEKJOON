0208984b6ffbc84291b5ee68b89f3e90a9fe12b3
Magin-a <80820166+Magin-a@users.noreply.github.com>
2021-11-12 23:04:39 +0900

    Add files via upload

---
+++ 구현/2002.py
@@ -0,0 +1,28 @@
+#추월 11/11~11.16
+import sys
+from collections import deque
+
+car_cnt = int(sys.stdin.readline())
+start = deque()
+finish = deque()
+result = 0
+site = -1
+
+for _ in range(car_cnt):
+    start.append(sys.stdin.readline().strip())
+
+for _ in range(car_cnt):
+    finish.append(sys.stdin.readline().strip())
+
+while start:
+    site += 1
+    a= start.popleft()
+    
+    if site <= finish.index(a):
+        pass
+    
+    else:
+        
+        result += 1
+
+print(result)

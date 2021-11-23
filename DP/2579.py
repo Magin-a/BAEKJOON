1ca3e099ebbf02654ac57c0ce95421fc868d4a2a
Magin-a <80820166+Magin-a@users.noreply.github.com>
2021-11-23 23:34:01 +0900

    계단오르기

--- DP/2579.py
+++ DP/2579.py
@@ -1,24 +1,26 @@
+#계단오르기
 import sys
 
-n = int(sys.stdin.readline())
-stair = []
+n = int(sys.stdin.readline()) #계단 갯수
+stair = [] #계단 저장
 
-for _ in range(n):
+for _ in range(n): #N개만큼 계단 생성
     stair.append(int(sys.stdin.readline()))
 
-d = [0]*n
+d = [0]*n #계단 갯수만큼 확보
 d[0] = stair[0]
 
-if n <3:
+if n <3: #계단의 갯수가 3개미만일때
     d[n-1] = sum(stair)
 
 else:
-    for x in range(1,n):
-        if x == 1:
-            d[x] = max(stair[x]+d[x-1], stair[x-1])
+    for x in range(1,3):
+        if x == 1: #두번째 계단
+            d[x] = max(stair[x]+d[x-1], stair[x-1]) #두번째 계단까지 올라가는 최고의 값
             continue
-        d[x] = max(stair[x]+stair[x-1], stair[x]+d[x-2])
+        d[x] = max(stair[x]+stair[x-1], stair[x]+d[x-2])#3번째 계단까지 올라가는 최고의 값
 
-    for i in range(3, n):
-        d[i] = max(stair[i]+stair[i-1]+ d[i-3], stair[i]+d[i-2])
+    for i in range(3, n): #4번째계단부터
+        d[i] = max(stair[i]+stair[i-1]+ d[i-3], stair[i]+d[i-2]) #i+1번째 계단까지 가는 최댓값
+
 print(d[-1])

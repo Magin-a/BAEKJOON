4ee4fb6d7ee73ff01e247191c63bd15904ce7087
Magin-a <80820166+Magin-a@users.noreply.github.com>
2021-12-25 11:53:30 +0900

    Update 1063.py

--- 구현/1063.py
+++ 구현/1063.py
@@ -1,38 +1,33 @@
 #킹
 import sys
-
 k, s, n = sys.stdin.readline().split()
 move_x, move_y = [0, 0, -1, 1, 1, 1, -1, -1], [1, -1, 0, 0, 1, -1, 1, -1]
 move_index = ['R', 'L', 'B', 'T', 'RT', 'LT', 'RB', 'LB']
-
-
 
 king_x, king_y = int(k[1]), ord(k[0])-64
 stone_x, stone_y = int(s[1]), ord(s[0])-64
 for _ in range(int(n)):
     num = move_index.index(sys.stdin.readline().strip())
+    test_x = king_x+move_x[num]
+    test_y = king_y+move_y[num]
+
+    if 1 > (test_x) or (test_x) > 8 or (test_y) <1 or (test_y) >8:
+        continue
+    
+    if test_x == stone_x and test_y == stone_y: 
+        t_stone_x = stone_x + move_x[num]
+        t_stone_y = stone_y + move_y[num]
+            
+        if 1 >t_stone_x or t_stone_x >8 or t_stone_y < 1 or t_stone_y > 8:
+            continue
+        
+        stone_x, stone_y = t_stone_x, t_stone_y
+    king_x, king_y = test_x, test_y
 
     
-    if 0 >= (king_x+move_x[num]) or (king_x+move_x[num]) > 8 or (king_y+move_y[num]) <=0 or (king_y+move_y[num]) >8:
-        pass
-    
-    else:
-        king_x += move_x[num]
-        king_y += move_y[num]
-        
-        
-        if king_x == stone_x and king_y == stone_y:
-            if 0 >= (stone_x+move_x[num]) or (stone_x+move_x[num]) > 8 or (stone_y+move_y[num]) <=0 or (stone_y+move_y[num]) >8:
-                
-                pass
-            else:
-                
-                stone_x +=  move_x[num]
-                stone_y += move_y[num]
-    
 
-print(chr(king_y+64), king_x) #
-print(chr(stone_y+64), stone_x)#
+print(chr(king_y+64), king_x)
+print(chr(stone_y+64), stone_x)
 
 
 

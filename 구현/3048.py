f19a3d65964e08fce9ee66e37cab536244b2a062
Magin-a <80820166+Magin-a@users.noreply.github.com>
2022-01-19 14:58:24 +0900

    개미

--- 구현/3048.py
+++ 구현/3048.py
@@ -1,23 +1,31 @@
 #개미
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 g1_cnt, g2_cnt = map(int, input().split())
-g1 = deque(map(str, input().rstrip()))
-g1.reverse()
-g2 = deque(map(str, input().rstrip()))
+g1 = list(reversed(input().rstrip())) #진행방향에 맞게 reverse
+g2 = list(input().rstrip())
 time = int(input())
 
-
-while time:
-    if len(g2) == 0:
-        break
-    test = g2.popleft()
-    g1.insert(len(g1)-time, test)
-    time -= 1
-
-
 result = g1 + g2
-for ant in result:
-    print(ant, end='')
+
+num = 0
+
+#시간 기준 
+while num < time:
+    #해당 시간초에 개미들의 변화
+    for i in result[:-1]: # 마지막 값은 result[-2]값 순서에서 처리함
+        x = result.index(i)
+        if i in g1: #순서를 뒤집는데 기준이 되는 g1
+            if result[x+1] in g2: 
+                result[x], result[x+1] = result[x+1], result[x]
+
+            else:
+                continue
+
+        else:
+            continue
+    num += 1 #다음 시간
+
+for p in result: #출력
+    print(p, end='')

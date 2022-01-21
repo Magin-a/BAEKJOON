a49d1d370b29cfe5a8dce4b317ffa05d5dbcf913
Magin-a <80820166+Magin-a@users.noreply.github.com>
2022-01-21 16:42:35 +0900

    거북이

--- 구현/8911.py
+++ 구현/8911.py
@@ -2,65 +2,55 @@
 import sys
 input = sys.stdin.readline
 
-test_c = int(input())
+test_c = int(input()) #테스트 갯수
 
 
-def move_d(i):
+def move_d(i): #방향 회전
     global cnt
 
     if i == 'L':
-        if cnt == 0:
-            cnt = len(move) - 1
-        
-        else:
-            cnt -= 1
+        cnt -= 1
+        cnt %= 4
 
     elif i == 'R':
-        if cnt == 3:
-            cnt = 0
-        else:
-            cnt += 1
+        cnt += 1
+        cnt %= 4
 
     return cnt
 
-def turtle_s(turtle, i):
+def turtle_s(turtle, i): #해당 방향으로 이동
     global cnt
     
     if i == 'B':
-        turtle[0] -= move[cnt][0]
-        turtle[1] -= move[cnt][1]
+        turtle[0] -= move_x[cnt]
+        turtle[1] -= move_y[cnt]
 
     else:
-        turtle[0] += move[cnt][0]
-        turtle[1] += move[cnt][1]
+        turtle[0] += move_x[cnt]
+        turtle[1] += move_y[cnt]
     
     return turtle
 
 for _ in range(test_c):
-    case = list(input().rstrip())
-    move = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    cnt = 0
-    turtle = [0, 0]
-    turtle_status = move[cnt]
-    result_x , result_y= [0, 0], [0, 0]#x좌표 최대최소, y좌표 최대최소
+    case = list(input().rstrip()) #이동 명령 부분
+    move_x = [0, 1, 0, -1] # 이동 x좌표
+    move_y = [1, 0, -1, 0] # 이동 y좌표
+    cnt = 0                # 방향 조정 변수
+    turtle = [0, 0]        #거북이의 현재 위치
+    result_x = [0, 0]      #최종 계산에 사용할 x좌표의 최대 최소
+    result_y = [0 ,0]      #최종 계산에 사용할 y좌표의 최대 최소
+    
 
     for i in case:
-        if i == 'L' or i == 'R':
+        if i == 'L' or i == 'R': 
             move_d(i)
 
         elif i == 'B' or i == 'F':
             turtle_s(turtle, i)
-            if turtle[0] > result_x[1]:
-                result_x[1] = turtle[0]
-
-            elif turtle[0] < result_x[0]:
-                result_x[0] = (turtle[0])
-
-            if turtle[1] > result_y[1]:
-                result_y[1] = turtle[1]
-
-            elif turtle[1] < result_y[0]:
-                result_y[0] = (turtle[1])
-            
-        result_x[0], result_y[0] = abs(result_x[0]), abs(result_y[0])
-    print(sum(result_x)*sum(result_y))
+            #이동 후 좌표 계산
+            result_x[0] = min(turtle[0], result_x[0]) 
+            result_x[1] = max(turtle[0], result_x[1])
+            result_y[0] = min(turtle[1], result_y[0])
+            result_y[1] = max(turtle[1], result_y[1])
+        
+    print((result_x[1]-result_x[0])*(result_y[1]-result_y[0])) #최종 출력

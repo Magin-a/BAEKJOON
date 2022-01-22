59e918761dcd1690524df973c4a55ba5f2fa51b1
Magin-a <80820166+Magin-a@users.noreply.github.com>
2022-01-22 23:54:12 +0900

    바이러스

--- 그래프/2606.py
+++ 그래프/2606.py
@@ -1,1 +1,14 @@
+#바이러스
+import sys
+input = sys.stdin.readline
 
+cnt = int(input())
+graph = [[] for _ in range(cnt)]
+
+test = int(input())
+for _ in range(test):
+    index, link = map(int, input().split())
+    graph[index-1].append(link-1)
+    graph[link-1].append(index-1)
+
+print(graph)

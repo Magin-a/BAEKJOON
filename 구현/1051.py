5e76f76c304b46161442e83e697a40e2259ce488
Magin-a <80820166+Magin-a@users.noreply.github.com>
2022-02-08 20:58:00 +0900

    숫자 정사각형

--- 구현/1051.py
+++ 구현/1051.py
@@ -1,9 +1,8 @@
-#도형정사각형
 #숫자 정사각형
 import sys
 from collections import deque
-input = sys.stdin.readline
 
+input = sys.stdin.readline
 n, m = map(int, input().split())
 
 graph = [list(map(int, input().rstrip())) for _ in range(n)]
@@ -16,28 +15,33 @@ def angle():
             if b not in find_num and a.count(b) >= 2:
                 find_num.append(b)
             continue
-        
         while find_num:
             test = find_num.popleft()
-            num_x1, num_x2 = a.index(test), a.index(test, -1)
-            # print('x',num_x1, num_x2)
-
-            y1, y2 = 0, 0
-            for _ in range(n-1):
-                y1 += 1
-                y2 += 1
-                if graph[y1][num_x1] == test and graph[y2][num_x2] == test:
-                    num_y2 = y2
-
-                else:
-                    continue
+            num_x1, num_x2 = a.index(test), a.index(test, a.index(test)+1)
+            w = num_x2 - num_x1
             
-            if (y2-y1) != (num_x1):
-                continue
-        
-        result = max(result, ((num_x2-num_x1)+1)*((num_y2-idx)+1))
+            if idx+w <n and test == graph[idx+w][num_x1] and test == graph[idx+w][num_x2]:
+                result = max(result, (w+1)**2)
             
-        return result
+    return result
 
 a = angle()
 print(a)
+
+#완료
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+g = [list(map(int, input().rstrip())) for _ in range(n)]
+
+
+check = min(n, m)
+answer = 0
+for i in range(n):
+    for j in range(m):
+        for k in range(check):
+            if ((i + k) < n) and ((j + k) < m) and (g[i][j] == g[i][j + k] == g[i + k][j] == g[i + k][j + k]): #같은 숫자 정사각형 확인
+                answer = max(answer, (k + 1)**2)
+                
+print(answer)

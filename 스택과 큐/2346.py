75adfbd01560b5eb32a8423718657b9ce1c70020
Magin-a <yg990828@naver.com>
2022-02-27 23:55:20 +0900

    push

--- 스택과 큐/2346.py
+++ 스택과 큐/2346.py
@@ -1,1 +1,20 @@
 #풍선 터트리기
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+q = deque(enumerate(map(int, input().split())))
+result =[]
+
+while q:
+    idx, data = q.popleft()
+    result.append(idx+1)
+
+    if data < 0:
+        q.rotate(-data)
+
+    elif data > 0:
+        q.rotate(data-1)
+
+print(result)

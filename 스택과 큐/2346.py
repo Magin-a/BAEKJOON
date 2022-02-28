6b6c3391b369d608a2d341d6d7831bc3e4231499
Magin-a <yg990828@naver.com>
2022-02-28 11:17:41 +0900

    push

--- 스택과 큐/2346.py
+++ 스택과 큐/2346.py
@@ -5,16 +5,13 @@ input = sys.stdin.readline
 
 n = int(input())
 q = deque(enumerate(map(int, input().split())))
-result =[]
 
 while q:
     idx, data = q.popleft()
-    result.append(idx+1)
+    print(idx+1, end=" ")
 
-    if data < 0:
+    if data < 0:# rotate(음수) => index = 0 값 뒤로 보내기 
         q.rotate(-data)
 
-    elif data > 0:
+    elif data > 0:# rotate(양수) => index = -1 앞으로 당기기
         q.rotate(-(data-1))
-
-print(" ".join(map(str, result)))

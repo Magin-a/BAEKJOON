a148c270ba244cfae5f5db141e274d3e23188403
Yeoungng Park <p87589580@gmail.com>
2022-03-06 01:10:49 +0900

    회전초밥 pypy

---
+++ Brute Force/1057.py
@@ -0,0 +1,4 @@
+#토너먼트
+import sys
+input = sys.stdin.readline
+
--- 투포인터/2531.py
+++ 투포인터/2531.py
@@ -5,28 +5,27 @@ input = sys.stdin.readline
 
 #1.회전초밥 수, 2.초밥의 가짓수, 3.연속해서 먹는 접시 수, 4.쿠폰 번호
 n, d, k, c= map(int, (input().split()))
-data = deque([int(input().rstrip()) for _ in range(n)])
-
+data = [int(input().rstrip()) for _ in range(n)]
+data.extend(data[:k])
 result = 0
-for i in range(n):
+start = 0
+
+while True:
     dish = []
-    end = 0
-    total = 0
-    while True:
-        if end >= k: #연속해서 먹은접시 목표량 달성 여부
-            if c not in dish: #쿠폰 초밥을 먹었는가 여부 확인
-                total += 1
-            break
+    end = start + k
 
-        if data[end] in dish:
-            break
+    if start > n-1:
+        break
 
-        dish.append(data[end])
-        total += 1
-        end += 1
-    
-    
-    result = max(result, total) #최대 값 갱신
-    data.rotate(-1) #초밥 회전
+    for p in range(k):
+        dish.append(data[start+p])
+
+
+    if c not in dish: #쿠폰 초밥을 먹었는가 여부 확인
+        dish.append(c)
+        
+    start += 1
+    result = max(result, len(set(dish))) #최대 값 갱신
 
 print(result) 
+#pypy 통과 

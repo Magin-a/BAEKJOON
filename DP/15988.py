b740268dc4ead50e18e707f772195bd6af8b5ec2
Magin-a <yg990828@naver.com>
2022-03-08 14:57:43 +0900

    1, 2, 3 더하기 3

--- DP/15988.py
+++ DP/15988.py
@@ -2,12 +2,10 @@
 import sys
 input = sys.stdin.readline
 
-goals = [1, 2, 4]
+goals = [1, 2, 4, 7] #1, 2, 4, 7, 13, 24, 44 ~
 
-
-for _ in range(int(input())):
-
+for _ in range(int(input())): #테스트 갯수
     n = int(input())
     for i in range(len(goals), n):
-        goals.append((goals[i-3] + goals[i-2] + goals[i-1])%1000000009)
-    print(goals[i])
+        goals.append((goals[i-3] + goals[i-2] + goals[i-1])%1000000009)  #점화식
+    print(goals[n-1])

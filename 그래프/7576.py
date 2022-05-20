410188a2e8c8b0f0452891489ff1672e2c421481
Magin-a <80820166+Magin-a@users.noreply.github.com>
2022-05-21 02:00:00 +0900

    토마토

--- 그래프/7576.py
+++ 그래프/7576.py
@@ -7,43 +7,37 @@ m, n = map(int ,input().split())
 graph = []
 q = deque()
 visited = []
+cnt = 0
 #1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않은 칸
-for _ in range(n):
+for a in range(n):
     graph.append(list(map(int, input().split())))
+
+    for b in range(m): #익은 토마토 큐에 저장
+        if graph[a][b]==1:
+            q.append([a,b])
 
 move_x = [-1, 1, 0, 0]
 move_y = [0, 0, -1, 1]
 
-def bfs(x, y, cnt):
-    q.append([x, y])
-    visited.append([x, y])
-    
+def bfs():
     while q:
         test_x, test_y = q.popleft()
-        cnt +=1
+
         for num in range(4):
             see_x = test_x + move_x[num]
             see_y = test_y + move_y[num]
 
-            if not(0 <= see_x < n) or not( 0 <= see_y < m):
-                continue
-            
-            if graph[see_x][see_y] !=0 or [see_x, see_y] in visited:
-                continue
-
-            
-            graph[see_x][see_y] = 1
-            q.append([see_x, see_y])
+            if 0 <= see_x < n and 0 <= see_y < m and graph[see_x][see_y] == 0:
+                q.append([see_x,see_y])
+                graph[see_x][see_y] = graph[test_x][test_y]+1
             
 
-    print(cnt)
-
-
-
-
-
-for a in range(n):
-    for b in range(m):
-        if graph[a][b] == 1 :
-            bfs(a, b, 0)
-
+bfs()
+cnt = 0
+for i in graph:
+    for j in i:
+        if j==0:
+            print(-1)
+            exit(0)
+    cnt = max(cnt,max(i))
+print(cnt-1)

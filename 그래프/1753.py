efa4f9aab1aac4e2c4b7d99f209a49082c77ed1f
Magin-a <80820166+Magin-a@users.noreply.github.com>
2022-06-30 16:02:40 +0900

    최단경로(다익스트라)

---
+++ 그래프/1753.py
@@ -0,0 +1,43 @@
+#최단경로 
+#그래프, 다익스트라
+import sys
+import heapq 
+
+input = sys.stdin.readline
+inf = int(1e9)
+v, e = map(int, input().split()) #정점, 간선
+graph = [[] for _ in range(v+1)]
+distance = [inf]*(v+1)
+
+start = int(input())
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a].append((b,c)) # b는 도착지 c 가중치
+
+def dijkstra(start):
+    q = []
+
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now =  heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, v+1):
+    if distance[i] == inf:
+        print('INF')
+
+    else:
+        print(distance[i])
